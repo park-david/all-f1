accb9f8d4ae53df563b12d62263f0266f71f5851
park-david <davidpark1094@gmail.com>
2023-08-14 09:36:19 -0700

    heroku setup

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,8 @@ app.use(cors())
 // body parser - used for POST/PUT/PATCH routes:
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+// use the React build folder for static files
+app.use(express.static(path.join(path.dirname(__dirname), 'frontend', 'dist')))
 
 
 /* Mount routes
@@ -34,6 +36,11 @@ app.use(express.json())
 // This tells our app to look at the `controllers/comments.js` file 
 // to handle all routes that begin with `localhost:3000/api/comments`
 app.use('/api/comments', commentsCtrl)
+
+// Any other route not matching the routes above gets routed by React
+app.get('*', (req, res) => {
+    res.sendFile(path.join(path.dirname(__dirname), 'frontend', 'dist', 'index.html'));
+});
 
 
 /* Tell the app to listen on the specified port
