72d1b7ea45913ab5755d75d2e05969d9acbbd171
park-david <davidpark1094@gmail.com>
2023-09-18 22:01:53 -0700

    changed state to update dependong on the date

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -1,15 +1,33 @@
-import { Carousel } from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-import CommentThread from '../CommentThread'
-import { useState } from 'react'
-import { Card } from 'semantic-ui-react'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import CommentThread from '../CommentThread';
+import { useState, useEffect } from 'react';
+import { Card } from 'semantic-ui-react';
 
 export default function HomePage({ circuitsData }) {
-    const [currentSlide, setCurrentSlide] = useState(0)
+    const [currentSlide, setCurrentSlide] = useState(0);
 
     const CarouselChange = (index) => {
-        setCurrentSlide(index)
-    }
+        setCurrentSlide(index);
+    };
+
+    // Find the index of the upcoming circuit based on the current date
+    const findUpcomingCircuitIndex = () => {
+        const currentDate = new Date();
+        for (let i = 0; i < circuitsData.length; i++) {
+            const circuitDate = new Date(circuitsData[i].date);
+            if (circuitDate > currentDate) {
+                return i;
+            }
+        }
+        return 0; // Default to the first circuit if no upcoming circuit is found
+    };
+
+    useEffect(() => {
+        // Set the initial state to the upcoming circuit on component load
+        const upcomingCircuitIndex = findUpcomingCircuitIndex();
+        setCurrentSlide(upcomingCircuitIndex);
+    }, [circuitsData]); // Use circuitsData as a dependency to update when the data changes
 
     return (
         <>
@@ -31,5 +49,5 @@ export default function HomePage({ circuitsData }) {
                 </div>
             </div>
         </>
-    )
+    );
 }
